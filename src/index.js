68dd3dfc261cacc2e46ca33082f09a01ab0af368
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-03-25 23:59:55 +0100

    added clean React project with typescript

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// support for IE9+
-import 'react-app-polyfill/ie9';
-
-import React from 'react';
-import './index.scss';
-import App from './App';
-import {render} from 'react-snapshot';
-
-render(<App/>, document.getElementById('root'));
