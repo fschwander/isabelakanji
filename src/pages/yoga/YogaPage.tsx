69d0db62b19bb16ecc9f99aa24c0888f8c8ef767
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-03-26 20:53:48 +0100

    scaling background gradient in main page (not in header)

--- src/pages/yoga/YogaPage.tsx
+++ src/pages/yoga/YogaPage.tsx
@@ -1,24 +1,53 @@
 import './YogaPage.scss';
-import React, {useState} from "react";
+import React, {createRef, RefObject, useEffect, useState} from "react";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
 import ogImage from '../../res/imgs/ogImage.jpg';
 import {Helmet} from 'react-helmet';
 import {Header} from "./components/Header";
+import * as d3 from "d3-scale";
 
 export const YogaPage: React.FC = () => {
   const [scrollTop, setScrollTop] = useState(0);
+  const rootRef: RefObject<HTMLDivElement> = createRef();
+  const [backgroundGradient, setBackgroundGradient] = useState()
 
   const url = 'http://www.isabelakanji.ch';
   const title = 'Isabel Akanji | Yoga';
   const description = 'Sanfte Hatha Yoga-Lektionen mit Isabel Akanji';
 
-  const handleScroll = (e:any) => {
+  const handleScroll = (e: any) => {
     setScrollTop(e.target.scrollTop);
+    scaleColorOnScroll()
   }
 
+  const scaleColorOnScroll = () => {
+      const lightBlue = '#93BED5';
+      const darkBlue = '#4791B8';
+      const lightGreen = '#9CC7C8';
+      const darkGreen = '#5BA2A4';
+
+      const maxY = 300;
+
+      // @ts-ignore
+      const topColorScale = d3.scaleLinear().domain([0, maxY]).range([darkGreen, darkBlue]);
+      // @ts-ignore
+      const bottomColorScale = d3.scaleLinear().domain([0, maxY]).range([lightBlue, lightGreen]);
+
+      let topColor = (value: number) => value <= maxY ? topColorScale(value) : topColorScale(maxY);
+      let bottomColor = (value: number) => value <= maxY ? bottomColorScale(value) : bottomColorScale(maxY);
+
+      setBackgroundGradient(`linear-gradient(to bottom, ${topColor(scrollTop)} 10%, ${bottomColor(scrollTop / 4)} 90%)`)
+  }
+
+  useEffect(() => {
+    setBackgroundGradient(scaleColorOnScroll)
+  }, [scrollTop])
+
   return (
-    <div className={'YogaPage'} onScroll={handleScroll}>
+    <div className={'YogaPage'} ref={rootRef}
+         onScroll={handleScroll}
+         style={{backgroundImage: backgroundGradient}}>
       <Helmet>
         <title>{title}</title>
         <meta name='description' content={description}/>
--- src/pages/yoga/components/Header.tsx
+++ src/pages/yoga/components/Header.tsx
@@ -10,10 +10,9 @@ interface HeaderInterface {
 export const Header: React.FC<HeaderInterface> = props => {
   const rootElement: RefObject<HTMLDivElement> = React.createRef();
   const logoContainerElement: RefObject<HTMLDivElement> = React.createRef();
-  const topBackgroundElement: RefObject<HTMLDivElement> = React.createRef();
+  const {scrollTop} = props;
 
   const handleScroll = () => {
-    scaleColorOnScroll();
     scaleSizeOnScroll();
     scaleOpacityOnScroll();
   }
@@ -21,69 +20,33 @@ export const Header: React.FC<HeaderInterface> = props => {
   const scaleSizeOnScroll = () => {
 
     if (rootElement.current && logoContainerElement.current) {
-      let scrollY = props.scrollTop;
       let maxY = rootElement.current.offsetHeight * 0.9;
-      let yPos = (scrollY <= maxY ? scrollY : maxY) / 4;
+      let yPos = (scrollTop <= maxY ? scrollTop : maxY) / 4;
 
       const sizeScale = d3.scaleLinear().domain([0, 200]).range([1, 1.4]);
-      logoContainerElement.current.style.transform = `scale(${sizeScale(scrollY)}) translateY(${yPos}px)`;
+      logoContainerElement.current.style.transform = `scale(${sizeScale(scrollTop)}) translateY(${yPos}px)`;
 
       // const sizeLogoScale = d3.scaleLinear().domain([0, 200]).range([1, 1.2]);
       // const logo = document.getElementById("logo-shape");
-      // logo.style.transform = `scale(${sizeLogoScale(scrollY)})`;
+      // logo.style.transform = `scale(${sizeLogoScale(scrollTop)})`;
       // logo.style.transformOrigin = `center`;}
     }
   }
 
   const scaleOpacityOnScroll = () => {
-    // let scrollY = window.scrollY;
-    // let maxY = rootElement.offsetHeight * 0.4;
-
+    // let maxY = rootElement.current.offsetHeight * 0.4;
+    //
     // const opacityScale = d3.scaleLinear().domain([maxY - 100, maxY]).range([1, 0]);
-    // logoContainerElement.style.opacity = opacityScale(scrollY);
-  }
-
-  const scaleColorOnScroll = () => {
-    // const lightBlue = '#93BED5';
-    // const darkBlue = '#4791B8';
-    // const lightGreen = '#9CC7C8';
-    // const darkGreen = '#5BA2A4';
-    //
-    // const maxY = 300;
-    // const scrollY = window.scrollY;
-
-    // const topColorScale = d3.scaleLinear().domain([0, maxY]).range([darkGreen, darkBlue]);
-    // const bottomColorScale = d3.scaleLinear().domain([0, maxY]).range([lightBlue, lightGreen]);
-
-    // let topColor = value => value <= maxY ? topColorScale(value) : topColorScale(maxY);
-    // let bottomColor = value => value <= maxY ? bottomColorScale(value) : bottomColorScale(maxY);
-    //
-    // topBackgroundElement.style.backgroundImage = `linear-gradient(to bottom, ${topColor(scrollY)} 10%, ${bottomColor(scrollY / 4)} 90%)`;
-    // logoContainerElement.style.border = `2px solid ${topColor(scrollY)}`;
+    // logoContainerElement.current.style.opacity = opacityScale(scrollTop);
   }
 
   useEffect(() => {
-      // // console.log(rootElement);
-      // if (rootElement.current) {
-      //   rootElement.current.addEventListener('scroll', handleScroll)
-      // }
-      // handleScroll()
-      // scaleColorOnScroll();
-      // return () => {
-      //   window.removeEventListener('scroll', handleScroll)
-      //
-      // }
-      scaleColorOnScroll()
       handleScroll()
     }, [props.scrollTop]
   )
 
-
   return (
     <div id='Header' className='Header' ref={rootElement}>
-
-      <span className='topBackgroundElement' ref={topBackgroundElement}/>
-
       <div className='logoContainer' ref={logoContainerElement}>
         <Logo/>
       </div>
