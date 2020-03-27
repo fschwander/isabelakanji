be200b26424c5b1b4d83ca82d05b07643f6feb9d
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-03-27 19:22:25 +0100

    updating DrawerNavContainer.tsx on resize

--- src/container/DrawerNavContainer.tsx
+++ src/container/DrawerNavContainer.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement, useState} from "react";
+import React, {ReactElement, useEffect, useState} from "react";
 import './DrawerNavContainer.scss';
 import {YogaPage} from "../pages/yoga/YogaPage";
 import {SchnapsPage} from "../pages/schnaps/SchnapsPage";
@@ -11,6 +11,7 @@ interface NavItem {
 
 export const DrawerNavContainer: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [registerWidth, setRegisterWidth] = useState(0)
   const navItems: Array<NavItem> = [
     {
       text: 'Yoga',
@@ -24,24 +25,27 @@ export const DrawerNavContainer: React.FC = () => {
     }
   ];
 
+  useEffect(() => {
+    const updateSize = () => {
+      setRegisterWidth(window.innerWidth * 0.04 < 30 ? 30 : window.innerWidth * 0.04);
+    };
+    window.addEventListener('resize', () => updateSize());
+    updateSize()
+  });
+
   return (
     <div className={`DrawerNavContainer`}>
       {navItems.map((item, i) => {
-          const registerWidth = 2;
-
           return (
-
             <div className={`drawer-item horizontal-container ${i === activeIndex ? 'active' : 'not-active button'}`}
                  style={{
                    backgroundColor: item.color,
-                   width: i === activeIndex ? `calc(100% - ${(navItems.length - 1) * registerWidth}rem)` : `${registerWidth}rem`
+                   width: i === activeIndex ? `calc(100% - ${(navItems.length - 1) * registerWidth}px)` : `${registerWidth}px`
                  }}
                  onClick={() => setActiveIndex(i)}
                  key={item.text + i}>
 
-              <div className={'drawer-label'}
-                   style={{minWidth: `${registerWidth}rem`}}
-              >
+              <div className={'drawer-label'} style={{minWidth: `${registerWidth}px`}}>
                 <h4>{item.text}</h4>
               </div>
               <div className={'drawer-page-container'}>
