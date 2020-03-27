3236a6c35058a3e257192dbe5ea2a1c361ccdfd6
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-03-27 18:47:34 +0100

    fixed false scaling of viewport width

--- src/container/DrawerNavContainer.tsx
+++ src/container/DrawerNavContainer.tsx
@@ -21,25 +21,29 @@ export const DrawerNavContainer: React.FC = () => {
       text: 'Schnaps',
       color: '#DDE2B5',
       component: <SchnapsPage/>
-    },
-    {
-      text: 'Foo',
-      color: '#E5B8B8',
-      component: <SchnapsPage/>
-    }];
+    }
+  ];
 
   return (
     <div className={`DrawerNavContainer`}>
       {navItems.map((item, i) => {
+          const registerWidth = 2;
 
           return (
 
             <div className={`drawer-item horizontal-container ${i === activeIndex ? 'active' : 'not-active button'}`}
-                 style={{backgroundColor: item.color}}
+                 style={{
+                   backgroundColor: item.color,
+                   width: i === activeIndex ? `calc(100% - ${(navItems.length - 1) * registerWidth}rem)` : `${registerWidth}rem`
+                 }}
                  onClick={() => setActiveIndex(i)}
                  key={item.text + i}>
 
-              <div className={'drawer-label'}>{item.text}</div>
+              <div className={'drawer-label'}
+                   style={{minWidth: `${registerWidth}rem`}}
+              >
+                <h4>{item.text}</h4>
+              </div>
               <div className={'drawer-page-container'}>
                 {item.component}
               </div>
